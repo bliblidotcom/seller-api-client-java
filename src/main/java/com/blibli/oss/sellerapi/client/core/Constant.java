0f3f2c8eea7b18eb043bc708901c844bd73ab219
mariaolivia <37130159+mariaolivia@users.noreply.github.com>
2019-12-13 11:45:09 +0700

    Update Constant.java
    add constant for http method type

--- src/main/java/com/blibli/oss/sellerapi/client/core/Constant.java
+++ src/main/java/com/blibli/oss/sellerapi/client/core/Constant.java
@@ -9,6 +9,9 @@ public class Constant {
   
   public static final String HTTP_GET = "GET";
   public static final String HTTP_POST = "POST";
+  public static final String HTTP_PUT = "PUT";
+  public static final String HTTP_PATCH = "PATCH";
+  public static final String HTTP_DELETE = "DELETE";
   
   public static final Integer DEFAULT_TIMEOUT = 15000;
   
